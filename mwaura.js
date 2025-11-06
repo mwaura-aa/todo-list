9683add97c8c92c7e1519b451580a55f6a232a19
mwaura-aa <136342954+mwaura-aa@users.noreply.github.com>
2025-11-06 10:40:08 +0300

    Add files via upload

---
+++ mwaura.js
@@ -0,0 +1,65 @@
+const inputContent = document.getElementById("input");
+const date = document.getElementById('date');
+const addBtn = document.getElementById('add');
+const display = document.getElementById('display');
+
+let inputArray = JSON.parse(localStorage.getItem('todos')) || [];
+
+addBtn.onclick = function (){
+  const todoTask = inputContent.value.trim();
+  const todoDate = date.value;
+
+  if(todoTask === '' || todoDate === ''){
+    alert("Please Enter a Todo and a Date");
+  }
+
+  inputArray.push({task: todoTask, date: todoDate});
+
+  localStorage.setItem('todos', JSON.stringify(inputArray));
+
+  inputContent.value = '';
+  date.value = '';
+  inputContent.focus();//  this puts the cursor back in the text box
+  
+  renderTask();
+}
+
+function renderTask(){
+  display.innerHTML = '';
+
+  for(let i = 0; i < inputArray.length; i++){
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('div-todoItem')
+    const todoText = document.createElement('span');
+    const todoDate = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('deleteBtn');
+
+    todoText.textContent = inputArray[i].task;
+    todoDate.textContent = inputArray[i].date;
+    deleteBtn.textContent = 'Delete';
+
+    deleteBtn.onclick = function(){
+      inputArray.splice(i, 1);
+      localStorage.setItem('todos', JSON.stringify(inputArray));
+      renderTask();
+    }
+
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(todoDate);
+    todoItem.appendChild(deleteBtn);
+
+    todoText.style.marginRight = '160px';
+    todoDate.style.marginRight = '70px'; 
+
+    display.appendChild(todoItem);
+  }
+}
+
+window.onload = renderTask;
+
+document.addEventListener('keydown', function(event){
+  if(event.key === 'Enter'){
+    addBtn.click();
+  }
+})
